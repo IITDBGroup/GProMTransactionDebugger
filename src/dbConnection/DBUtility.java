feebee8a7d4e987fb7f547a237985703d5cb4967
dcamposfaial <dcamposfaial@hawk.iit.edu>
2016-07-14 14:44:37 -0500

    new library added

--- src/dbConnection/DBUtility.java
+++ src/dbConnection/DBUtility.java
@@ -1,7 +1,8 @@
 package dbConnection;
 
 public interface DBUtility {
-	public static final String HOST = "104.194.103.92";
+
+	public static final String HOST = "127.0.0.1";
 	public static final String SID = "orcl";
 	public static final String PORT = "1521";
 	public static final String USERNAME = "hr";
---
+++ src/gui/transactionDebuggerFrame/graph/Tutorial1.java
@@ -0,0 +1,5 @@
+package gui.transactionDebuggerFrame.graph;
+
+public class Tutorial1 {
+
+}
